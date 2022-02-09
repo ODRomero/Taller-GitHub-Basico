24622a8791e40a48d4a724ec1dcda20f63635f50
Oscar Romero <99354132+ODRomero@users.noreply.github.com>
2022-02-09 19:23:38 +0100

    Agregando primera versión de FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+import java.util.stream.IntStream;
+ 
+public class FizzBuzz 
+{
+  public static void main(String[] args) 
+  {
+    fizzBuzz(100);
+  }
+ 
+  private static void fizzBuzz(int num) 
+  {
+    for (int i = 1; i <= num; i++) 
+    {
+      if (((i % 5) == 0) && ((i % 7) == 0)) 
+        System.out.println("fizzbuzz");
+      else if ((i % 5) == 0) 
+        System.out.println("fizz");
+      else if ((i % 7) == 0) 
+        System.out.println("buzz");
+      else
+        System.out.println(i); 
+    }
+  }
+ 
+
+}
